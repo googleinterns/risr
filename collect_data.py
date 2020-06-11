c5209c895040e50552d01b86e6c68f46b2afa50b
Elisa Zhao Hang <ezhaohang@gmail.com>
2020-06-11 12:29:37 -0400

    Updated collect_data.py to get PR comments.

--- collect_data.py
+++ collect_data.py
@@ -32,7 +32,7 @@ def get_intern_repos():
     """Retrieves all the STEP intern repos and stores them in a CSV file.
 
 
-    The CSV file has the following columns:
+    intern_repos.csv has the following columns:
         owner: The STEP intern Github username.
         name: The repository name.
         created: The time and date that the repository was created.
@@ -110,10 +110,146 @@ def get_intern_repos():
         writer.writerows(intern_repos)
 
 
+def get_pr_comments():
+    """ Retrieves the comments in pull requests for intern repositories.
+
+    pr_comment_counts.csv has the following columns:
+        pr_path: The resource path to the pull request.
+        created: The date and time that the pull request was created.
+        total_comments: Number of comments in pull request.
+
+    pr_comments.csv has the following columns:
+        comment_path: The resource path to the comment.
+        created: The date and time that the comment was created.
+        author: The Github username of the comment author.
+        comment: The text in the comment.
+
+    """
+    
+    def get_pr_comments_per_repo(name, owner):
+        """ Gets the pull request comments from a particular repository.
+
+        This function processes the results from the Github API query and
+        appends relevant information to the comment_counts list.
+
+        Args:
+            name: A string containing the repository name.
+            owner: A string containing the repository owner.
+
+        Returns:
+            None.
+        """
+
+        def process_comment(comment):
+            """ Helper function to process a comment.
+
+            This function updates the total_comments variable and appends
+            relevant information to the comment_list list.
+            
+            Args:
+                comment: The comment node retrieved from the API.
+            
+            Returns:
+                None.
+            """
+
+            nonlocal total_comments
+            if comment['body'] != "" and comment:
+                total_comments += 1
+
+                # For the special case where the author has been deleted
+                if not comment['author']:
+                    comment['author'] = {"login": "deleted-user"}
+                
+                comment_list.append(
+                    [comment['resourcePath'], comment['createdAt'],
+                     comment['author']['login'], comment['body']]
+                )
+            return
+
+        query = f"""{{
+            repository(name: "{name}", owner: "{owner}") {{
+                pullRequests(first: 20) {{
+                    nodes {{
+                        resourcePath
+                        createdAt
+                        comments(first: 20) {{
+                          nodes {{
+                            resourcePath
+                            body
+                            createdAt
+                            author {{
+                              login
+                            }}
+                          }}
+                        }}
+                        reviews(first: 20) {{
+                            nodes {{
+                                resourcePath
+                                body
+                                createdAt
+                                author {{
+                                    login
+                                }}
+                                comments(first: 20) {{
+                                    nodes {{
+                                        resourcePath
+                                        body
+                                        createdAt
+                                        author {{
+                                            login
+                                        }}
+                                    }}
+                                }}
+                            }}
+                        }}
+                    }}
+                }}
+            }}
+        }}"""
+
+        result = run_query(query)
+
+        pull_requests = result['data']['repository']['pullRequests']['nodes']
+
+        for pr in pull_requests:
+            total_comments = 0
+            for pr_comment in pr['comments']['nodes']:
+                process_comment(pr_comment)
+            for review in pr['reviews']['nodes']:
+                process_comment(review)
+                for review_comment in review['comments']['nodes']:
+                    process_comment(review_comment)
+            comment_counts.append(
+                [pr['resourcePath'], pr['createdAt'], total_comments]
+            )
+
+    comment_counts = []
+    comment_list = []
+
+    # Iterates through all repositories stored in the intern_repos.csv file
+    with open('intern_repos.csv', newline='') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            get_pr_comments_per_repo(row['name'], row['owner'])
+
+    with open('pr_comment_counts.csv', 'w', newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["pr_path", "created", "total_comments"])
+        writer.writerows(comment_counts)
+
+    with open('pr_comments.csv', 'w', newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["comment_path", "created", "author", "comment"])
+        writer.writerows(comment_list)
+
 def main():
 
     # Uncomment below to get new list of intern repositories.
     # get_intern_repos()
+
+    # Uncomment below to get new comments from pull requests.
+    # get_pr_comments()
 
 
 if __name__ == "__main__":
