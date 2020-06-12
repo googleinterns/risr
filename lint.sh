6798f25b9d113731275e57836fa7b667e33d4591
Elisa Zhao Hang <ezhaohang@gmail.com>
2020-06-11 21:21:27 -0400

    Added comment for duplicate-code in lint.sh and removed pypy3.

--- lint.sh
+++ lint.sh
@@ -31,7 +31,9 @@ function lintChangedFiles () {
 set -o errexit
 set -o nounset
 
-SKIP_FOR_CODE="duplicate-code"
+# This line prevents linter from detecting duplicate code, which is necessary
+# because there are multiple closing brackets across files that look the same.
+SKIP_FOR_CODE="duplicate-code" 
 SKIP_FOR_TESTS="redefined-outer-name,protected-access,missing-docstring,too-many-lines,len-as-condition"
 SKIP_FOR_SNIPPETS="${SKIP_FOR_TESTS},reimported,unused-variable,unused-import,import-outside-toplevel"
 
