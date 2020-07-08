a936da97906c74de56cee9f70e5a83ddf3a7b36b
ezhaohang <30475421+ezhaohang@users.noreply.github.com>
2020-07-08 17:05:59 -0400

    Added licence text to all files and addressed linter errors.

--- risr-app/risr_proj/risr_proj/wsgi.py
+++ risr-app/risr_proj/risr_proj/wsgi.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https: // www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 WSGI config for risr_proj project.
 
@@ -13,4 +28,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'risr_proj.settings')
 
+# pylint: disable=C0103
 application = get_wsgi_application()
