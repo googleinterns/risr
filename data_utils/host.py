d61826a7f962b2963911b27bb67e07cf2329a78a
Elisa Zhao Hang <ezhaohang@gmail.com>
2020-08-06 23:38:22 -0400

    Revert "Added code to get additional hosts based on starter repository pull request reviewers."
    This reverts commit 6660471b1b90bc30466f87591cf20e9069f6fdb6.

--- data_utils/host.py
+++ data_utils/host.py
@@ -18,98 +18,6 @@
 import csv
 import os
 import sys
-from query import run_query
-
-
-def get_pr_reviewers(name, owner):
-    """ Gets the reviewer usernames for a particular repository.
-
-    This method processes the results from the Github PAI query.
-
-    Args:
-        name: A string containing the repository name.
-        owner: A string containing the repository owner.
-
-    Returns:
-        A JSON object with the pull request reviewer information.
-    """
-
-    query = f"""{{
-        repository(name: "{name}", owner: "{owner}") {{
-            pullRequests(first: 5) {{
-                nodes {{
-                    resourcePath
-                    author {{
-                        login
-                    }}
-                    timelineItems(first: 100) {{
-                        nodes {{
-                            ... on ReviewRequestedEvent {{
-                                requestedReviewer {{
-                                    ... on User {{
-                                        login
-                                    }}
-                                }}
-                            }}
-                            ... on PullRequestReview {{
-                                author {{
-                                    login
-                                }}
-                            }}
-                        }}
-                    }}
-                }}
-            }}
-        }}
-    }}"""
-
-    return run_query(query)
-
-
-def process_reviewer_query_results(result, host_dict, intern_usernames):
-    """ Processes the query results for the reviewer usernames.
-
-    Updates host_dict if new host usernames are found. Assumes that reviewers
-    in starter projects are hosts.
-
-    Args:
-        result: The results from the query.
-        host_dict: Dictionary containing host information.
-        intern_usernames: Set of known intern usernames.
-
-    Returns:
-        None.
-    """
-
-    try:
-        pull_requests = (result["data"]["repository"]["pullRequests"]["nodes"])
-    except:
-        raise Exception(
-            "Query results for reviewers does not have a structure that is"
-            " currently supported by RISR.")
-
-    for pull_request in pull_requests:
-        reviewers = pull_request["timelineItems"]["nodes"]
-        for reviewer in reviewers:
-            if not reviewer:
-                continue
-            if "requestedReviewer" in reviewer:
-                host = reviewer["requestedReviewer"]
-            if "author" in reviewer:
-                host = reviewer["author"]
-            # Special case in which host account has been deleted
-            try:
-                host_username = host["login"]
-            except KeyError:
-                continue
-
-            # Check if host is already in dictionary and if intern reviewed
-            # their own pull request.
-            if host_username not in host_dict and \
-                host_username not in intern_usernames:
-                # Start date and team number are unknown in this case.
-                host_dict[host_username] = ["unknown", "unknown"]
-
 
 def main():
     """ Saves host usernames from STEP teams CSV.
@@ -122,52 +30,25 @@ def main():
     """
 
     try:
-        teams_file = sys.argv[1]
+        filename = sys.argv[1]
     except:
         raise Exception("Usage: host.py <STEP teams CSV>")
 
-    if not os.path.isfile(teams_file):
+    if not os.path.isfile(filename):
         raise Exception("The CSV for the Github usernames does not exist.")
 
-    host_dict = dict()
-
-    # Get host information from the STEP team CSV
-    with open(teams_file, newline="") as in_csv:
+    with open(filename, newline="") as in_csv, \
+         open("data/host_usernames.csv", "w", newline="") as out_csv:
         reader = csv.DictReader(in_csv)
+        writer = csv.writer(out_csv)
+        writer.writerow(["username", "start_date", "team"])
         for row in reader:
             start_date = row["Start Date"]
             team = row["Team Number"]
-            host1 = row["Github username 1"]
-            host2 = row["Github username 2"]
-            if host1:
-                host_dict[host1] = [start_date, team]
-            if host2:
-                host_dict[host2] = [start_date, team]
-
-    # Store list of intern usernames based on owners of starter project
-    # repositories.
-    intern_usernames = set()
-
-    # Check starter repositories for users that approved pull requests
-    with open("data/repos.csv", newline="") as in_csv:
-        reader = csv.DictReader(in_csv)
-        for row in reader:
-            if row["repo_type"] == "capstone":
-                continue
-            intern_usernames.add(row["owner"])
-            query_results = get_pr_reviewers(row["name"], row["owner"])
-            process_reviewer_query_results(
-                query_results,
-                host_dict,
-                intern_usernames
-            )
-
-
-    with open("data/host_usernames.csv", "w", newline="") as out_csv:
-        writer = csv.writer(out_csv)
-        writer.writerow(["username", "start_date", "team"])
-        for host in host_dict:
-            writer.writerow([host, host_dict[host][0], host_dict[host][1]])
+            if row["Github username 1"]:
+                writer.writerow([row["Github username 1"], start_date, team])
+            if row["Github username 2"]:
+                writer.writerow([row["Github username 2"], start_date, team])
 
 
 if __name__ == "__main__":
