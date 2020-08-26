427ecd8c79442ef128b4674f41104217f7341969
Elisa Zhao Hang <ezhaohang@gmail.com>
2020-08-26 12:08:42 -0400

    Add start date and review count for PRs.

--- data_utils/pr_stats.py
+++ data_utils/pr_stats.py
@@ -17,6 +17,7 @@
 import csv
 import os
 import sys
+from datetime import datetime
 from query import run_query
 
 
@@ -35,8 +36,13 @@ def get_pr_stats(name, owner):
 
     query = f"""{{
         repository(name: "{name}", owner: "{owner}") {{
-            pullRequests(first: 50) {{
+            pullRequests(first: 100) {{
                 nodes {{
+                    participants(first: 10) {{
+                        nodes {{
+                            login
+                        }}
+                    }}
                     resourcePath
                     number
                     createdAt
@@ -54,6 +60,13 @@ def get_pr_stats(name, owner):
                             }}
                         }}
                     }}
+                    timelineItems(first: 100) {{
+                        nodes {{
+                            ... on PullRequestReview {{
+                                state
+                            }}
+                        }}
+                    }}
                 }}
             }}
         }}
@@ -63,35 +76,57 @@ def get_pr_stats(name, owner):
     try:
         pull_requests = result["data"]["repository"]["pullRequests"]["nodes"]
         return pull_requests
-    except KeyError:
+    except (KeyError, TypeError):
         print(f"PR statistics results for {owner}/{name} does not have a"
               "structure that is currently supported by RISR.")
         return []
 
 
-def process_stats_query_results(writer, pull_requests):
+def process_stats_query_results(writer, pull_requests, host_dict):
     """ Processes the query results for pull request statistics.
 
     Uses the writer to record the pull request statistics information.
 
     Args:
         writer: CSV writer to record the data in a CSV file.
-        result: the results from the query.
+        pull_requests: the results from the query.
+        host_dict: dictionary with host logins as keys and start dates as
+                   values.
 
     Returns:
         None.
     """
 
     for pull_request in pull_requests:
+        participants = pull_request["participants"]["nodes"]
+        start_date = "unknown"
+        for user in participants:
+            # Special case in which host account has been deleted
+            try:
+                cur_username = user["login"]
+            except (TypeError, KeyError):
+                continue
+            if cur_username in host_dict:
+                start_date = host_dict[cur_username]
+                break
         total_comments = pull_request["comments"]["totalCount"]
         for review in pull_request["reviews"]["nodes"]:
             if review["body"] != "" and review:
                 total_comments += 1
             total_comments += review["comments"]["totalCount"]
+        created_date = datetime.fromisoformat(pull_request["createdAt"][:-1])
+        created_date = created_date.strftime("%-m/%-d/%Y")
+
+        review_count = 0
+        for item in pull_request["timelineItems"]["nodes"]:
+            if not item:
+                continue
+            review_count += 1
+
         writer.writerow([
             pull_request["resourcePath"], pull_request["number"],
-            pull_request["createdAt"], pull_request["closedAt"],
-            total_comments,
+            start_date, created_date,
+            total_comments, review_count,
             pull_request["deletions"] + pull_request["additions"]
         ])
 
@@ -131,17 +166,25 @@ def main():
     if not os.path.isfile(repo_csv):
         raise Exception("The CSV for repositories does not exist.")
 
+    # Keep dictionary of known host - start date mappings.
+    host_dict = dict()
+    with open("data/host_info.csv", newline="") as in_csv:
+        reader = csv.DictReader(in_csv)
+        for row in reader:
+            host_dict[row["username"]] = row["start_date"]
+
+
     with open(repo_csv, newline="") as in_csv, \
             open(stats_csv, "w", newline="") as out_csv:
         reader = csv.DictReader(in_csv)
         writer = csv.writer(out_csv)
         writer.writerow([
-            "pr_path", "pr_number", "created", "closed", "total_comments",
-            "pr_lines_changed"
+            "pr_path", "pr_number", "start_date", "created_date",
+            "total_comments", "review_count", "pr_lines_changed"
         ])
         for row in reader:
             query_results = get_pr_stats(row["name"], row["owner"])
-            process_stats_query_results(writer, query_results)
+            process_stats_query_results(writer, query_results, host_dict)
 
 
 if __name__ == "__main__":
